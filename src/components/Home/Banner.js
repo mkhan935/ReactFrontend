12c6c7c70da42d4793dacaab5c4dc50faac025af
Mohammed Khan <m.khan935@gmail.com>
2017-07-04 20:35:17 -0400

    src code added

---
+++ src/components/Home/Banner.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Banner = ({ appName, token }) => {
+  if (token) {
+    return null;
+  }
+  return (
+    <div className="banner">
+      <div className="container">
+        <h1 className="logo-font">
+          {appName.toLowerCase()}
+        </h1>
+        <p>A place to make fun of your friends.</p>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
